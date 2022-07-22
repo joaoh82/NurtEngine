81d94f87692cdf7ab007f51bde2eb5296c0a1279
João Henrique Machado Silva <joaoh82@gmail.com>
2022-07-22 11:12:26 +0200

    Added Premake and Build Steps to the project

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "NurtEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "NurtEngine"
+	location "NurtEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"NE_PLATFORM_WINDOWS",
+			"NE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		symbols "On"
+
+	filter "configurations:Debug"
+		defines "HZ_DIST"
+		symbols "On"
+		
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"NurtEngine/vendor/spdlog/include",
+		"NurtEngine/src"
+	}
+
+	links 
+	{
+		"NurtEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"NE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		symbols "On"
+
+	filter "configurations:Debug"
+		defines "HZ_DIST"
+		symbols "On"
