9968fcec0d5ae6b6e392f4e78605558b9ebbe67f
João Henrique Machado Silva <joaoh82@gmail.com>
2022-08-26 20:33:09 +0200

    Implemented main window events

--- NurtEngine/src/NurtEngine/Application.cpp
+++ NurtEngine/src/NurtEngine/Application.cpp
@@ -1,15 +1,18 @@
 #include "nepch.h"
 #include "Application.h"
 
-#include "NurtEngine/Events/ApplicationEvent.h"
 #include "NurtEngine/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace NurtEngine {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
@@ -25,6 +28,24 @@ namespace NurtEngine {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		// If event is a WindowCloseEvent, then calls OnWindowClose event
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		// Logging events for debugging purposes
+		NE_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		// This will terminate the application on the next loop
+		m_Running = false;
+
+		return true;
 	}
 
 }
--- NurtEngine/src/NurtEngine/Application.h
+++ NurtEngine/src/NurtEngine/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "NurtEngine/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace NurtEngine {
@@ -13,7 +15,12 @@ namespace NurtEngine {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
